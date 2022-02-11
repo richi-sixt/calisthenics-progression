ac48ce9423af7037ae2621209a6d60b8576af3a0
richi-sixt <richi.sixt@gmail.com>
2022-02-11 19:22:56 +0100

    update config

--- README.md
+++ README.md
@@ -10,8 +10,27 @@ Python 3.9.4
 
 Install requirements.txt in virtual environement
 
-create .env file in root directory
+> `pip install -r requirements.txt`
+
+create .env file in root directory (will be invoked in \_config.py):
+
+```python
+WTF_CSRF_SECRET_KEY="your-secret-key"
+SECRET_KEY="an-other-secret-key-which-is-hard-to-guess"
+DB_URI="db-url-for-example-postgresql://localhost/db"
+SECURITY_PASSWORD_SALT="another-salt-for-the-password"
+FLASK_ENV="development"
+
+MAIL_SERVER="smtp.yourmailprovider.com"
+MAIL_PORT="587"
+MAIL_USE_TLS="1"
+MAIL_USERNAME="username"
+MAIL_PASSWORD="password"
+MAIL_DEFAULT_SENDER="email-for-the-sender"
+```
 
 export run settings in terminal:
 
 > `export FLASK_APP=calisthenics_progression.py`<br> `export FLASK_ENV=development`
+
+## Features
--- project/_config.py
+++ project/_config.py
@@ -13,28 +13,26 @@ MYSQL_CURSORCLASS = 'DictCursor'
 WORKOUTS_PER_PAGE = 10
 SQLALCHEMY_DATABASE_URI = os.getenv('DB_URI')
 # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
-#         "sqlite:///" + os.path.join(basedir, "app.db")
+#         "sqlite:///" + os.path.join(basedir, "app.db") 
 SQLALCHEMY_POOL_RECYCLE = 299
 SECURITY_PASSWORD_SALT = os.getenv('SECURITY_PASSWORD_SALT')
 FLASK_ENV = os.getenv('FLASK_ENV')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-# mail settings
-# MAIL_SERVER = 'b1.tophost.ch'
-# MAIL_PORT = 465
-# MAIL_USE_TLS = False
-# MAIL_USE_SSL = True
-
-#gmail authentification
+#Mail authentification
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_PORT = os.environ.get('MAIL_PORT')
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
 
 # mail accounts
-MAIL_SERVER="smtp.googlemail.com"
-MAIL_PORT="587"
-MAIL_USE_TLS="1"
-MAIL_USERNAME="richi.sixt@gmail.com"
-MAIL_PASSWORD="vrnohegaasrrultb"
-MAIL_DEFAULT_SENDER="richi.sixt@gmail.com"
-# MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+# MAIL_SERVER="smtp.googlemail.com"
+# MAIL_PORT="587"
+# MAIL_USE_TLS="1"
+# MAIL_USERNAME="richi.sixt@gmail.com"
+# MAIL_PASSWORD="vrnohegaasrrultb"
+# MAIL_DEFAULT_SENDER="richi.sixt@gmail.com"
+# # MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
