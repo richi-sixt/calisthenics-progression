1d9187264c0dc57805ef89318cbf7c4223a22a00
richi-sixt <richi.sixt@gmail.com>
2022-02-13 00:38:04 +0100

    log_stdout in config

--- project/config.py
+++ project/config.py
@@ -6,17 +6,17 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 #main config
 class Config(object):
-    DEBUG = True
     WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     WORKOUTS_PER_PAGE = 10
     # replace for heroku-version of postgresql
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
-
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT')
     FLASK_ENV = os.environ.get('FLASK_ENV')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
 
     #Mail authentification
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
