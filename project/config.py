961c554c9a677a0577c3a3e63aba0caaf50ba66b
richi-sixt <ruechan.sixt@proton.me>
2026-02-21 21:01:36 +0100

    int MAIL_PORT

--- project/config.py
+++ project/config.py
@@ -27,7 +27,7 @@ class Config(object):
     # Mail authentification
     # APP_MAIL_SERVER in production -> hosted environements overwrites MAIL_SERVER
     MAIL_SERVER = os.environ.get("APP_MAIL_SERVER") or os.environ.get("MAIL_SERVER")
-    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT", 25))
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
     MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL")
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
