27ab55c37f32ea53dce309d360af27f006a6e7b2
richi-sixt <ruechan.sixt@proton.me>
2026-02-21 18:54:28 +0100

    increase password_hash column size to 512 for MySQL

--- project/models.py
+++ project/models.py
@@ -28,7 +28,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(512))
 
     # User status fields
     admin = db.Column(db.Boolean, nullable=False, default=False)
