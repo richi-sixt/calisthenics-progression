b7037b34692074640e87a6bc9be9c71ed1c17d29
richi-sixt <ruechan.sixt@proton.me>
2026-02-21 22:22:45 +0100

    added error/log handling for async_email

--- project/email.py
+++ project/email.py
@@ -11,7 +11,27 @@ from project import mail
 def send_async_email(app, msg: Message) -> None:
     """Send an email inside the given Flask application context."""
     with app.app_context():
-        mail.send(msg)
+        try:
+            mail.send(msg)
+            app.logger.info(
+                "Email sent to %s | subject: %s | server: %s:%s | tls=%s ssl=%s",
+                msg.recipients,
+                msg.subject,
+                app.config.get("MAIL_SERVER"),
+                app.config.get("MAIL_PORT"),
+                app.config.get("MAIL_USE_TLS"),
+                app.config.get("MAIL_USE_SSL"),
+            )
+        except Exception:
+            app.logger.exception(
+                "Failed to send email to %s | subject: %s | server: %s:%s | tls=%s ssl=%s",
+                msg.recipients,
+                msg.subject,
+                app.config.get("MAIL_SERVER"),
+                app.config.get("MAIL_PORT"),
+                app.config.get("MAIL_USE_TLS"),
+                app.config.get("MAIL_USE_SSL"),
+            )
 
 
 def send_email(
