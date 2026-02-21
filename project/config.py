2d44b443d3e75db49f40f099469b02e8fb04333d
richi-sixt <ruechan.sixt@proton.me>
2026-02-21 20:36:06 +0100

    use APP_MAIL_SERVER as env-var for production

--- project/config.py
+++ project/config.py
@@ -25,9 +25,11 @@ class Config(object):
     ADMINS = os.environ.get("ADMINS")
 
     # Mail authentification
-    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    # APP_MAIL_SERVER in production -> hosted environements overwrites MAIL_SERVER
+    MAIL_SERVER = os.environ.get("APP_MAIL_SERVER") or os.environ.get("MAIL_SERVER")
     MAIL_PORT = os.environ.get("MAIL_PORT")
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
+    MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL")
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
