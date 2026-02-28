ff9dc795972cf2110a499c5874209456efd8bf41
richi-sixt <ruechan.sixt@proton.me>
2026-02-28 19:59:25 +0100

    fix CodeQL error URL redirection from remote source

--- project/auth/routes.py
+++ project/auth/routes.py
@@ -42,7 +42,12 @@ def login() -> ResponseReturnValue:
             return redirect(url_for("auth.login"))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
-        if not next_page or urlsplit(next_page).netloc != "":
+        if next_page:
+            next_page = next_page.replace("\\", "")
+            parsed = urlsplit(next_page)
+            if parsed.netloc or parsed.scheme:
+                next_page = None
+        if not next_page:
             next_page = url_for("main.index")
         return redirect(next_page)
     return render_template("auth/login.html", title="Anmelden", form=form)
